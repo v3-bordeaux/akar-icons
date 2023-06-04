cc579231440b84a05f9df59d0fcb020d0dd312ef
Arthaud <arthaudp33@gmail.com>
2023-06-04 12:22:40 +0200

    fix: parking icon

--- src/icons/parking.js
+++ src/icons/parking.js
@@ -18,7 +18,7 @@ const Parking = ({ color = 'currentColor', size = '24', ...otherProps }) => {
       <g clipPath="url(#clip0_7_2148)">
         <circle cx="12" cy="12" r="11"></circle>
       </g>
-      <path d="M9 17.5v-4.2m0 0h4.286c1.07 0 3.214 0 3.214-3.15S14.357 7 13.286 7H9v6.3z"></path>
+      <path d="M9.5 16.5v-3.6m0 0h3.428c.857 0 2.572 0 2.572-2.7s-1.715-2.7-2.572-2.7H9.5v5.4z"></path>
       <defs>
         <clipPath id="clip0_7_2148">
           <rect width="24" height="24"></rect>
